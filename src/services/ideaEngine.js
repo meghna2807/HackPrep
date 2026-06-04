10301721c64a1ae35df3bf93474991b9c0a5c6ac
meghna2807 <meghnachauhan2807@gmail.com>
2026-06-04 16:24:13 +0530

    fix alias processing

--- src/services/ideaEngine.js
+++ src/services/ideaEngine.js
@@ -1,6 +1,19 @@
 const ideas = require("../data/ideas");
 
+
 function generateIdeas(domain){
+    const aliases = {
+    finance: "fintech",
+    banking: "fintech",
+    health: "healthcare",
+    medical: "healthcare"
+    };
+
+    if(aliases[domain]){
+        domain = aliases[domain];
+    }
+
+
     return ideas[domain] || [];
 }
 
